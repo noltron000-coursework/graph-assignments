20881dec009a9a8ecb71ead9101a9b3eac0e9af6
Nolan Kovacik <noltron000@gmail.com>
2019-08-06 21:03:40 -0700

    fixing up challenge 4
    still needs good comments etc

--- challenges/challenge_4.py
+++ challenges/challenge_4.py
@@ -1,46 +1,95 @@
-def sort_backpack(capacity, items):
+def sort_knapsack(items, capacity):
 	'''
-	this is a simple way to pick the best items first.
-	its a solution that does *not* use dynamic programming.
-	a solution is found in O(nlog(n)) time.
+	"capacity" is a numeric value representing a max weight.
+	this capacity will be modified as items are added.
+	---
+	"storage" is a list (usually a set) of item tuples.
+	the tuples inside store an item's name, value, and weight.
+	this variable represents all possible items available.
+	---
+	"knapsack" is a list (usually a set) of item tuples.
+	the tuples inside store an item's name, value, and weight.
+	this variable represents desireable items to pack up.
 	'''
-	# the packed set is all the items we know we want to pack.
-	packed = set()
-	# items are sorted by their value and weight,
-	# putting the lightest & most valuable items at the front.
-	items = sorted(items, key=lambda val: val[1]/val[2])
-	# once sorted, the best ratios are added first.
-	for item in items:
-		name = item[0]
-		weight = item[1]
-		value = item[2]
-		# if the item can fit, it is added.
-		if weight <= capacity:
-			capacity -= weight
-			packed.add(item)
-	# the packed set can be deconstructed later.
-	print(packed)
-	return packed
+	# storage starts full of items with unknown desireabilities.
+	# storage holds undesireable items & starts empty.
+	storage = items.copy()
+	# knapsack holds desireable items & starts empty.
+	knapsack = set()
+	# as our knapsack fills up, its capacity will decrease.
+	print(pack_decision(storage, knapsack, capacity))
+
+def pack_decision(storage, knapsack, capacity):
+	# not sure if these are needed...
+	storage = storage.copy()
+	knapsack = knapsack.copy()
+
+	# base case - knapsack is full or storage is empty.
+	if capacity == 0 or len(storage) == 0:
+		return knapsack
+
+	# extract current item
+	item = storage.pop()
+	# create semantic names
+	item_name = item[0]
+	item_value = item[1]
+	item_weight = item[2]
+
+	if item_weight > capacity:
+		# item's weight is too heavy to fit in knapsack.
+		return pack_decision(storage, knapsack, capacity)
+
+	else:
+		# imagine a reality where the current item is added in.
+		new_cap = capacity - item_weight
+		new_knapsack = knapsack.copy()
+		new_knapsack.add(item)
+		new_knapsack = pack_decision(storage, new_knapsack, new_cap)
+		# imagine a reality where the current item is left out.
+		old_knapsack = pack_decision(storage, knapsack, capacity)
+
+		# calculate total values of both backpacks.
+		# its O(n), but can be easily improved with refactor.
+		def get_value(backpack):
+			result = 0
+			for column in (row[1] for row in backpack):
+				result += column
+			return result
+		# use function call to calculate sums.
+		old_value = get_value(old_knapsack)
+		new_value = get_value(new_knapsack)
+		print("CHECK IT")
+		print(old_value)
+		print(new_value)
+
+		# check which scenario is better.
+		if new_value >= old_value:
+			# knapsack.add(item)
+			# capacity -= item_weight
+			return new_knapsack
+
+		elif new_value < old_value:
+			return old_knapsack
 
 
 if __name__ == '__main__':
-	# a given capacity of the backpack
-	capacity = 100
+	# a given capacity of the knapsack
+	capacity = 50
 
 	# a list of items w/properties, in no particular order
-	# items = {
-	# 	('boot', 10, 60),
-	# 	('tent', 20, 100),
-	# 	('water', 30, 120),
-	# 	('first aid', 15, 70),
-	# }
-
-	# BUG this scenario does not work
+	# properties are: name, value, weight -- in order.
 	items = {
-		('yesterday\'s trash', 50, 1),
-		('silver ingot', 100, 100),
-		('bag of gold coins', 50, 98),
+		('boot', 60, 10),
+		('tent', 100, 20),
+		('water', 120, 30),
+		('first aid', 70, 15),
 	}
 
-	# sort the backpack
-	sort_backpack(capacity, items)
+	# # BUG this scenario does not work
+	# items = {
+	# 	('yesterday\'s trash', 1, 50),
+	# 	('silver ingot', 100, 100),
+	# 	('bag of gold coins', 98, 50),
+	# }
+	# sort the knapsack
+	sort_knapsack(items, capacity)
