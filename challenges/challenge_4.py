f3eea8cf0d0c9db1047df7e4c3de8323bfa1a68e
Nolan Kovacik <noltron000@gmail.com>
2019-08-06 18:53:50 -0700

    found an edge case

--- challenges/challenge_4.py
+++ challenges/challenge_4.py
@@ -2,16 +2,13 @@ def sort_backpack(capacity, items):
 	'''
 	this is a simple way to pick the best items first.
 	its a solution that does *not* use dynamic programming.
-	---
-	awkwardly enough, this problem has been encountered
-	in a certain video game called skyrim.
-	incidently, gems have the best value:weight ratio!
+	a solution is found in O(nlog(n)) time.
 	'''
-	# the packed set is all the items we want to pack
+	# the packed set is all the items we know we want to pack.
 	packed = set()
-	# items are sorted by their value and weight
-	items = sorted(items, key=lambda val: val[2]/val[1])
-	items.reverse()
+	# items are sorted by their value and weight,
+	# putting the lightest & most valuable items at the front.
+	items = sorted(items, key=lambda val: val[1]/val[2])
 	# once sorted, the best ratios are added first.
 	for item in items:
 		name = item[0]
@@ -21,18 +18,29 @@ def sort_backpack(capacity, items):
 		if weight <= capacity:
 			capacity -= weight
 			packed.add(item)
-	# the packed set can be deconstructed later
+	# the packed set can be deconstructed later.
 	print(packed)
 	return packed
 
 
 if __name__ == '__main__':
-	capacity = 50
+	# a given capacity of the backpack
+	capacity = 100
+
+	# a list of items w/properties, in no particular order
+	# items = {
+	# 	('boot', 10, 60),
+	# 	('tent', 20, 100),
+	# 	('water', 30, 120),
+	# 	('first aid', 15, 70),
+	# }
+
+	# BUG this scenario does not work
 	items = {
-		('boot', 10, 60),
-		('tent', 20, 100),
-		('water', 30, 120),
-		('first aid', 15, 70),
+		('yesterday\'s trash', 50, 1),
+		('silver ingot', 100, 100),
+		('bag of gold coins', 50, 98),
 	}
 
+	# sort the backpack
 	sort_backpack(capacity, items)
